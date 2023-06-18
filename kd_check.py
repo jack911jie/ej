0e3c33be5d7811ed042914b0278824deeef0552e
jack@new_mi <jack911jie@qq.com>
2023-06-18 11:26:36 +0800

    feat 批量处理同一天的多笔发送果园的订单

--- kd_check.py
+++ kd_check.py
@@ -20,10 +20,11 @@ import numpy as np
 
 
 class FruitKd:
-    def __init__(self,chromedriver_path='D:/Program Files (x86)/ChromeWebDriver/chromedriver'):
-        # pass
-        
-        self.driver=webdriver.Chrome(chromedriver_path)
+    def __init__(self,chromedriver_path):
+        if chromedriver_path:
+            self.driver=webdriver.Chrome(chromedriver_path)
+        else:
+            pass
 
         #无界面模式
         # self.driver=webdriver.Chrome(chromedriver_path,options=chrome_options)
@@ -225,13 +226,13 @@ class FruitKd:
 
         return df_dl_order_new['联系电话收货人'].tolist()
 
-
-    def order_to_guoyuan(self,dl_xls='E:\\temp\\ejj\\团购群\\订单\\0614导出.xlsx',output_dir='e:\\temp\\ejj\\团购群\\给果园的订单'):
+    def order_to_guoyuan(self,dl_xls='E:\\temp\\ejj\\团购群\\订单\\0614导出.xlsx',output_dir='e:\\temp\\ejj\\团购群\\给果园的订单',expand_accounts='yes',exp='yes'):
         df_order=pd.read_excel(dl_xls,sheet_name='订单列表')
         df_order_out=df_order.copy()
         df_order_out=df_order_out[['收货人','联系电话','详细地址','商品','订单号']]
         df_order_out.rename(columns={'收货人':'收件人姓名','联系电话':'收件人手机','详细地址':'收件人地址','商品':'品名'},inplace=True)
         df_order_out['收件人手机']=df_order_out['收件人手机'].apply(lambda x: str(x))
+        df_order_out['规格']=df_order_out['品名'].apply(lambda x: re.findall(r'(\d\d斤装|\d斤装)',x)[0])
         df_order_out['数量']=df_order_out['品名'].apply(lambda x: int(str(x).split('+')[-1]))
         df_order_out['发件人姓名']='团团好果'
         df_order_out['发件人手机']='18077796420'
@@ -240,20 +241,38 @@ class FruitKd:
         df_order_out['发件人单位']=''
         df_order_out['收件人电话']=''
         df_order_out['收件人单位']=''
-        df_order_out['规格']=''
+
+        # df_order_out['规格']=''
+        
+        
         df_order_out['备注']=''
         df_order_out['代收金额']=''
         df_order_out['到付金额']=''
 
         df_order_out=df_order_out[['发件人姓名','发件人手机','发件人电话','发件人地址','发件人单位','收件人姓名','收件人手机','收件人电话','收件人地址','收件人单位','品名','规格','数量','备注','订单号','代收金额','到付金额']]
 
-        xlsname=dl_xls.split('\\')[-1].split('.')[0].split('-')
-        datetxt,num=xlsname[0],xlsname[2]
-        fn='团团好果'+datetxt[4:6]+'.'+datetxt[6:]+'订单'+'-'+num+'.xlsx'
-        fn=os.path.join(output_dir,fn)
-        df_order_out.to_excel(fn,index=False)
-        print('导出完成')
-        self.guoyuan_xlsx_style(xls=fn)
+      
+        if expand_accounts=='yes':
+            df_repeated=df_order_out.loc[df_order_out.index.repeat(df_order_out['数量'])]
+            df_repeated=df_repeated.reset_index(drop=True)
+            df_repeated['数量']=1
+            df_res=df_repeated
+        else:
+            df_order_out['数量']=df_order_out['品名'].apply(lambda x: int(str(x).split('+')[-1]))
+            df_res=df_order_out
+
+
+        # print(df_repeated)
+        if exp=='yes':
+            xlsname=dl_xls.split('\\')[-1].split('.')[0].split('-')
+            datetxt,num=xlsname[0],xlsname[2]
+            fn='团团好果'+datetxt[4:6]+'.'+datetxt[6:]+'订单'+'-'+num+'.xlsx'
+            fn=os.path.join(output_dir,fn)
+            df_res.to_excel(fn,index=False)
+            print('导出完成')
+            self.guoyuan_xlsx_style(xls=fn)
+            os.startfile(output_dir)
+        return df_res
 
     def guoyuan_xlsx_style(self,xls):
         wb=openpyxl.load_workbook(xls)
@@ -292,6 +311,35 @@ class FruitKd:
         
         wb.save(xls)
         print('修改格式完成')
+
+    def multi_order_to_guoyuan(self,date=20230618,input_dir='E:\\temp\\ejj\\团购群\\订单',output_dir='e:\\temp\\ejj\\团购群\\订单\\给果园的订单',expand_accounts='yes',save_each_exp='no'):
+        date=str(date)
+        try:
+            fns=[]
+            for fn in os.listdir(input_dir):
+                if re.match(date+'-导出订单-\d\d.xlsx',fn):
+                    fns.append(os.path.join(input_dir,fn))
+
+            dfs=[]
+            for fn in fns:
+                df_res=self.order_to_guoyuan(dl_xls=fn,output_dir=output_dir,expand_accounts=expand_accounts,exp=save_each_exp)
+                if df_res.shape[0]>0:
+                    dfs.append(df_res)
+
+            df_concat=pd.concat(dfs)
+
+            fn='团团好果'+date[4:6]+'.'+date[6:]+'订单（'+str(len(fns))+'个订单合并）.xlsx'
+            fn=os.path.join(output_dir,fn)
+            df_concat.to_excel(fn,index=False)
+            print('导出完成')
+            self.guoyuan_xlsx_style(xls=fn)
+            os.startfile(output_dir)
+
+            return df_concat
+
+        except Exception as e:
+            print(e)
+
 
     def write_xlsx_back_kd(self,input_xls='e:\\temp\\ejj\\团购群\\订单\\给果园的订单\\团团好果06.17订单-01.xlsx',
                             out_dir='e:\\temp\\ejj\\团购群\\订单\\带物流信息的回传文件',
@@ -334,22 +382,49 @@ class FruitKd:
         self.close_chrome_driver()
         
 if __name__=='__main__':
-    p=FruitKd(chromedriver_path='')
+    
  
-    # 一、从快团团批量导入订单处理后生成给果园的订单。
+    # 一、从快团团批量导入订单处理后生成给果园的订单。只能处理一个文件。
+    p=FruitKd(chromedriver_path='')
+    # rs=p.order_to_guoyuan(dl_xls='E:\\temp\\ejj\\团购群\\订单\\20230618-导出订单-02.xlsx',
+    #                             output_dir='e:\\temp\\ejj\\团购群\\订单\\给果园的订单',
+    #                             expand_accounts='yes')
+    #参数说明：
+    # dl_xls：从快团团批量导出的订单，文件名修改为：20230618-导出订单-02.xlsx 的格式
+    # output_dir：生成给果园的订单文件后存放的文件夹
+    # expand_accounts：对于购买多于1件的商品，根据实际数量生成相应的记录。例如同一客户购买了3件，同一条记录生成3条，防止商家发货漏单。
+    # print(rs)
 
-        # rs=p.order_to_guoyuan(dl_xls='E:\\temp\\ejj\\团购群\\订单\\20230617-导出订单-02.xlsx',
-        #                          output_dir='e:\\temp\\ejj\\团购群\\订单\\给果园的订单')
-        # print(rs)
+    #批量处理同一天的不同订单，可将多个订单生成一个合并发货清单文件给果园。
+    rs=p.multi_order_to_guoyuan(date=20230622,
+                                input_dir='E:\\temp\\ejj\\团购群\\订单',
+                                output_dir='e:\\temp\\ejj\\团购群\\订单\\给果园的订单',
+                                expand_accounts='yes',
+                                save_each_exp='no')
+
+    #参数说明：
+    # dl_xls：从快团团批量导出的订单，文件名修改为：20230618-导出订单-02.xlsx 的格式
+    # output_dir：生成给果园的订单文件后存放的文件夹
+    # expand_accounts：对于购买多于1件的商品，根据实际数量生成相应的记录。例如同一客户购买了3件，同一条记录生成3条，防止商家发货漏单。
+    # each_exp：处理到每个订单时，是否分别保存。默认为no，即只保存最后生成的合并订单。
+    # print(rs)
 
     # 二、果园返单后，通过下载快团团模板文件查询快递单号并写入待上传文件
-    
-    res=p.write_xlsx_back_kd(input_xls='e:\\temp\\ejj\\团购群\\订单\\wuliu2023-06-17 21_27_45.xlsx.xlsx',
-                            out_dir='e:\\temp\\ejj\\团购群\\订单\\带物流信息的回传文件',
-                            url='http://kd.dh.cx/df66d',
-                            kd_name='申通快递',
-                            method='download')
-    print(res)
+    # p=FruitKd(chromedriver_path='D:/Program Files (x86)/ChromeWebDriver/chromedriver')
+    # res=p.write_xlsx_back_kd(input_xls='e:\\temp\\ejj\\团购群\\订单\\wuliu2023-06-17 21_27_45.xlsx.xlsx',
+    #                         out_dir='e:\\temp\\ejj\\团购群\\订单\\带物流信息的回传文件',
+    #                         url='http://kd.dh.cx/df66d',
+    #                         kd_name='申通快递',
+    #                         method='download')
+    # print(res)
+
+    #参数说明：
+    # input_xls：从快团团导出的待回传清单
+    # output_dir：生成给果园的订单文件后存放的文件夹
+    # url：查询地址，可能会经常改变
+    # kd_name: 快递公司名称。写入生成的回传清单中。
+    # method: download——从快团团导出的待回传清单文件查询物流单号并生成回传文件以上传。目前仅支持这一选项。
+
 
 
 
